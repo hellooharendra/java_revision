fcb679fe7e81fbfafa3575dd12c426d356b70bc1
Harendraprajapati72 <harendraprajapati72@gmail.com>
2022-08-10 01:31:49 +0530

    Constructor

---
+++ javaConstructor.java
@@ -0,0 +1,8 @@
+class Harendra{
+    Harendra(){
+        System.out.println(" \nit is default/nn-parameterised constructor \n");
+    }
+    public static void main(String[] args) {
+        Harendra h1=new Harendra();
+    }
+}
